a4040f84e90771f47a61cee4daa28f47c435cbb9
Chris Matthieu <chris@matthieu.us>
2017-11-28 10:12:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { get, set, remove } = require("ipts")
+const { exec } = require('child_process');
+const domain = "computes.a"
+const run = async () => {
+  let hashObj = await get(domain)
+  console.log(hashObj.hash)
+  exec('open http://ipfs.io/ipfs/' + hashObj.hash, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  });
+}
+run()
