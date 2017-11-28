ebd5cc83796e6057b55847666ad4e2f2b4c3d00d
Chris Matthieu <chris@matthieu.us>
2017-11-28 14:41:12 -0700

    created arpadyne website

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const { get, set, remove } = require("ipts")
-const { exec } = require('child_process');
-const domain = "computes.a"
-const run = async () => {
-  let hashObj = await get(domain)
-  console.log(hashObj.hash)
-  exec('open http://ipfs.io/ipns/' + hashObj.hash, (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(stdout);
-  });
-}
-run()
+
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+app.get('/ipts/:domain', async (req, res) => {
+  // console.log(req.params.domain);
+  const ipts = await get(req.params.domain)
+  res.send(ipts.hash)
+})
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
