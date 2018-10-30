6cda1161d0d0bdb41a83221a01cb82c279b14e5e
Chris Matthieu <chris@matthieu.us>
2018-10-29 18:02:47 -0700

    updated libraries

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const ipfs = new IPFS({
       Swarm: [
         // Use IPFS dev signal server
         // '/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star',
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
+        // '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
         // Use local signal server
         // '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star',
       ]
