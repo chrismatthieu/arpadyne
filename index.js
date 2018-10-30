60dc95d8799a4ddc512e519dfb4c75c95fe4eedd
Chris Matthieu <chris@matthieu.us>
2018-10-29 18:57:23 -0700

    testing on ssl

--- index.js
+++ index.js
@@ -28,15 +28,16 @@ const ipfs = new IPFS({
     pubsub: true,
   },
   config: {
-    Addresses: {
-      Swarm: [
-        // Use IPFS dev signal server
-        // '/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star',
-        // '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
-        // Use local signal server
-        // '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star',
-      ]
-    },
+    // Addresses: {
+    //   Swarm: [
+    //     // Use IPFS dev signal server
+    //     // '/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star',
+    //     '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
+    //     // Use local signal server
+    //     // '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star',
+    //   ]
+    // }
+    Bootstrap: []
   }
 })
 
