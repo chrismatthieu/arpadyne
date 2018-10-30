480cbaa160df0ce04a2915760c0623503f47ee91
Chris Matthieu <chris@matthieu.us>
2018-10-29 19:19:10 -0700

    try it now

--- index.js
+++ index.js
@@ -25,19 +25,18 @@ const ipfs = new IPFS({
   repo: '/orbitdb/examples/browser/new/ipfs/0.27.3',
   start: true,
   EXPERIMENTAL: {
-    pubsub: true,
+    pubsub: true
   },
   config: {
-    // Addresses: {
-    //   Swarm: [
+    Addresses: {
+      Swarm: [
     //     // Use IPFS dev signal server
     //     // '/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star',
-    //     '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
     //     // Use local signal server
     //     // '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star',
-    //   ]
-    // }
-    Bootstrap: []
+      ]
+    }
   }
 })
 
@@ -56,7 +55,7 @@ ipfs.once('ready', () => ipfs.id((err, info) => {
   const initDB = async () => {
     // Create / Open a database
     // db = await orbitdb.keyvalue('domains')
-
+    console.log("Connecting CRDT");
     db = await orbitdb.open("/orbitdb/QmSEjFUcS6Dyrk2S1y9D5sxcA8cUbh6XsVAZNf1VxugUQm/domains3", {
       // If database doesn't exist, create it
       create: true,
